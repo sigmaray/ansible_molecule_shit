f8bb5ad02dd27b321c6104c225b02ee9cc9403fc
Stanislav Zaleski <sigmaray@gmail.com>
2023-08-10 13:32:56 +0600

    .

--- roles/ansible_molecule_shit/files/make-symlinks.py
+++ roles/ansible_molecule_shit/files/make-symlinks.py
@@ -1,21 +1,18 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3.9
 
 import os
 import pathlib
 from datetime import date
 import uuid
 
-from_to_array = [
+folders_links = [
     [".config/google-chrome", "r/symlinks/.config/google-chrome"],
     [".config/google-chrome-beta", "r/symlinks/.config/google-chrome-beta"],
     [".ViberPC", "r/symlinks/.ViberPC"],
-    [".gitconfig", "r/symlinks/.gitconfig"],
-    [".gitignore", "r/symlinks/.gitignore"],
-    [".git-credentials", "r/symlinks/.git-credentials"],
     # ["_", "r/symlinks/_"],
 ]
 
-for from_to in from_to_array:
+for from_to in folders_links:
     h = os.getenv('HOME') + "/"
     hfrom = h + from_to[0]
     hto = h + from_to[1]
@@ -29,6 +26,36 @@ for from_to in from_to_array:
 
         # create destination dir if it does not exist
         pathlib.Path(hto).mkdir(parents=True, exist_ok=True)
+
+        # If file/dir exists and it's not a symink, make a backup (rename file/dir)
+        if os.path.exists(hfrom):
+            os.rename(hfrom, hfrom + ".bak." +
+                      date.today().strftime("%Y-%b-%d") + "." + uuid.uuid4().hex)
+
+        # create symlink
+        os.symlink(hto, hfrom)
+
+files_links = [
+    [".gitconfig", "r/symlinks/.gitconfig"],
+    [".gitignore", "r/symlinks/.gitignore"],
+    [".git-credentials", "r/symlinks/.git-credentials"],
+    # ["__", "r/symlinks/__"]
+]
+
+for from_to in files_links:
+    h = os.getenv('HOME') + "/"
+    hfrom = h + from_to[0]
+    hto = h + from_to[1]
+
+    # if from path is not a symlink
+    if not (os.path.islink(hfrom)):
+        # create parent dir if it does not exist
+        path = pathlib.Path(hfrom)
+        parent = path.parent
+        parent.mkdir(parents=True, exist_ok=True)
+
+        # create destination file if it does not exist
+        pathlib.Path(hto).touch()
 
         # If file/dir exists and it's not a symink, make a backup (rename file/dir)
         if os.path.exists(hfrom):
